6a12038ebd71f111e17ebb36c4a67dd48b3034e3
Domingos Lamas Rodrigues <domingoslamas@gmail.com>
2026-05-31 14:31:13 +0100

    Small fix and included Klippscreen.conf

--- install.sh
+++ install.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-repo=klipper_addon_de
+repo=DE_Beta_printer_configs
 repo_path="$(cd "$(dirname "$0")" && pwd)"
 
 # Exit if root
@@ -18,6 +18,7 @@ module_name5="nozzle_wipe.cfg"
 
 copy_module_name1="printer.cfg"
 copy_module_name2="wifi"
+copy_module_name3="KlipperScreen.conf"
 
 klipper_config_path="$HOME/printer_data/config/"
 
@@ -31,6 +32,36 @@ ln -sf "$repo_path/$module_name5" "${klipper_config_path}${module_name5}"
 # Copying modifiable files
 cp -f "$repo_path/$copy_module_name1" "${klipper_config_path}${copy_module_name1}"
 cp -f "$repo_path/$copy_module_name2" "${klipper_config_path}${copy_module_name2}"
+cp -f "$repo_path/$copy_module_name3" "${klipper_config_path}${copy_module_name3}"
+
+blk_path=~/printer_data/config/moonraker.conf
+# Include update block in moonraker.conf
+if [ -f "$blk_path" ]; then
+    if ! grep -q "^\[update_manager $repo\]$" "$blk_path"; then
+        sudo service moonraker stop
+        sed -i "\$a \ " "$blk_path"
+        sed -i "\$a [update_manager $repo]" "$blk_path"
+        sed -i "\$a type: git_repo" "$blk_path"
+        sed -i "\$a path: $repo_path" "$blk_path"
+        sed -i "\$a origin: https://github.com/DLR3D/printer_configs.git" "$blk_path"
+        sed -i "\$a primary_branch: Beta" "$blk_path"
+        sed -i "\$a is_system_service: False" "$blk_path"
+        sudo service moonraker start
+    else
+        echo "Including [update_manager] aborted, [update_manager] already exists in $blk_path"
+    fi
+
+    if ! grep -q "^\[update_manager $repo\]$" "$blk_path"; then
+        sudo service moonraker stop
+        sed -i "\$a \ " "$blk_path"
+        sed -i "\$a [update_manager klipper]" "$blk_path"
+        sed -i "\$a channel: stable" "$blk_path"
+        sed -i "\$a pinned_commit: a0d2769639493c4344546d922d40c30d2b121b91:" "$blk_path"
+        sudo service moonraker start
+    else
+        echo "Including [update_manager] aborted, [update_manager] already exists in $blk_path"
+    fi
+fi
 
 echo "Installation successful restarting klipper now."
 sudo service klipper stop
